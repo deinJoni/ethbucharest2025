5650cec1b525b88796fdfb1b44fab16dc6800094
adlipeeters <adlipeeters@gmail.com>
2025-04-03 16:08:49 +0300

    Add initial frontend configuration with ESLint, Tailwind CSS, and OnchainKit integration; update package dependencies and restructure project files

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,103 +1,138 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
+import {
+  ConnectWallet,
+  Wallet,
+  WalletDropdown,
+  WalletDropdownLink,
+  WalletDropdownDisconnect,
+} from '@coinbase/onchainkit/wallet';
+import {
+  Address,
+  Avatar,
+  Name,
+  Identity,
+  EthBalance,
+} from '@coinbase/onchainkit/identity';
+import ArrowSvg from './svg/ArrowSvg';
+import ImageSvg from './svg/Image';
+import OnchainkitSvg from './svg/OnchainKit';
+import { useAccount } from 'wagmi';
+
+const components = [
+  {
+    name: 'Transaction',
+    url: 'https://onchainkit.xyz/transaction/transaction',
+  },
+  { name: 'Swap', url: 'https://onchainkit.xyz/swap/swap' },
+  { name: 'Checkout', url: 'https://onchainkit.xyz/checkout/checkout' },
+  { name: 'Wallet', url: 'https://onchainkit.xyz/wallet/wallet' },
+  { name: 'Identity', url: 'https://onchainkit.xyz/identity/identity' },
+];
+
+const templates = [
+  { name: 'NFT', url: 'https://github.com/coinbase/onchain-app-template' },
+  { name: 'Commerce', url: 'https://github.com/coinbase/onchain-commerce-template' },
+  { name: 'Fund', url: 'https://github.com/fakepixels/fund-component' },
+];
+
+export default function App() {
+  const { address } = useAccount();
+  console.log(address);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="flex flex-col min-h-screen font-sans dark:bg-background dark:text-white bg-white text-black">
+      <header className="pt-4 pr-4">
+        <div className="flex justify-end">
+          <div className="wallet-container">
+            <Wallet>
+              <ConnectWallet>
+                <Avatar className="h-6 w-6" />
+                <Name />
+              </ConnectWallet>
+              <WalletDropdown>
+                <Identity className="px-4 pt-3 pb-2" hasCopyAddressOnClick>
+                  <Avatar />
+                  <Name />
+                  <Address />
+                  <EthBalance />
+                </Identity>
+                <WalletDropdownLink
+                  icon="wallet"
+                  href="https://keys.coinbase.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Wallet
+                </WalletDropdownLink>
+                <WalletDropdownDisconnect />
+              </WalletDropdown>
+            </Wallet>
+          </div>
+        </div>
+      </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <main className="flex-grow flex items-center justify-center">
+        <div className="max-w-4xl w-full p-4">
+          <div className="w-1/3 mx-auto mb-6">
+            <ImageSvg />
+          </div>
+          <div className="flex justify-center mb-6">
+            <a target="_blank" rel="_template" href="https://onchainkit.xyz">
+              <OnchainkitSvg className="dark:text-white text-black" />
+            </a>
+          </div>
+          <p className="text-center mb-6">
+            Get started by editing
+            <code className="p-1 ml-1 rounded dark:bg-gray-800 bg-gray-200">app/page.tsx</code>.
+          </p>
+          <div className="flex flex-col items-center">
+            <div className="max-w-2xl w-full">
+              <div className="flex flex-col md:flex-row justify-between mt-4">
+                <div className="md:w-1/2 mb-4 md:mb-0 flex flex-col items-center">
+                  <p className="font-semibold mb-2 text-center">
+                    Explore components
+                  </p>
+                  <ul className="list-disc pl-5 space-y-2 inline-block text-left">
+                    {components.map((component, index) => (
+                      <li key={index}>
+                        <a
+                          href={component.url}
+                          className="hover:underline inline-flex items-center dark:text-white text-black"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {component.name}
+                          <ArrowSvg />
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="md:w-1/2 flex flex-col items-center">
+                  <p className="font-semibold mb-2 text-center">
+                    Explore templates
+                  </p>
+                  <ul className="list-disc pl-5 space-y-2 inline-block text-left">
+                    {templates.map((template, index) => (
+                      <li key={index}>
+                        <a
+                          href={template.url}
+                          className="hover:underline inline-flex items-center dark:text-white text-black"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          {template.name}
+                          <ArrowSvg />
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
