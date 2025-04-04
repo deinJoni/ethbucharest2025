422dbcce90fb4eb240c63200043371f1ee8736d7
Valeriucoding <valeriucelmare1@gmail.com>
2025-04-04 20:30:17 +0300

    add starlette

--- apps/backend/main.py
+++ apps/backend/main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 
 from routes import token_metrics
 from core.config import settings
@@ -19,8 +19,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allow_headers=["Content-Type", "Authorization", "X-Requested-With"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Create database tables
